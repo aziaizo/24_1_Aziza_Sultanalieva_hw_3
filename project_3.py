9892ca802828c7a0bd893083e998b731e3e8749b
Aziza <118680875+aziaizo@users.noreply.github.com>
2022-12-24 16:56:01 +0600

    commit

--- project_3.py
+++ project_3.py
@@ -1,18 +1,23 @@
+import asyncio
+
 from aiogram.utils import executor
-from hendlers import callback,client,admin,fsmAdminMentor
+from hendlers import callback,client,admin,fsmAdminMentor,notifications
 import logging
 from config import disp
 from database.bot_db import sql_create
 
+
 fsmAdminMentor.register_handlers_fsm_admin_mentor(disp)
 
 client.register_hendlers_client(disp)
+notifications.register_handlers_notification(disp)
 admin.register_handlers_admin(disp)
 callback.register_hendlers_callback(disp)
 
-
 async def on_startup(_):
+    asyncio.create_task(notifications.scheduler())
     sql_create()
+
 
 
 if __name__ == '__main__':
